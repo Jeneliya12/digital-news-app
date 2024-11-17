02d00d2c3884712874a23af94bc759b0a4636d34
Jeneliya12 <gurungjeneliya@gmail.com>
2024-11-17 00:14:01 -0500

    add home icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,11 +5,17 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav id="navbar">
+    <nav id="navbar" className="navbar">
       <ul>
         {navs.map((nav) => (
           <li key={nav.id}>
-            <Link href={nav.link}>{nav.name}</Link>
+            <Link href={nav.link}>
+              {nav.name === "Home" ? (
+                <i className="bi bi-house-door-fill"></i>
+              ) : (
+                nav.name
+              )}
+            </Link>
           </li>
         ))}
       </ul>
