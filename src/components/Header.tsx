ee3efa1be78bc54d8471b13f85ccb35bd1a0a794
Jeneliya12 <gurungjeneliya@gmail.com>
2024-11-16 23:30:54 -0500

    add logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./header.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header id="header" className="header d-flex align-items-center fixed-top">
+      <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+        <a href="/" className="logo d-flex align-items-center">
+          {/* <img src="" alt="" /> */}
+          <h1>DigitalNews</h1>
+        </a>
+      </div>
+    </header>
+  );
 }
