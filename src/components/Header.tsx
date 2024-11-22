6b6e91edf37e0807224874716ff815389eb4d5e6
Jeneliya12 <gurungjeneliya@gmail.com>
2024-11-22 11:35:49 -0600

    add toggle button for mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,17 @@ import SearchForm from "./SearchForm";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
+  const [on, setOn] = useState(false);
 
   const handleFormOpen = (e: Event | any) => {
     e.preventDefault();
     setOpen(!open);
+  };
+
+  const handleToggleMenu = () => {
+    setOn(!on);
+    let body: HTMLElement | any = document.querySelector("body");
+    body.classList.toggle("mobile-nav-active");
   };
 
   return (
@@ -27,6 +34,18 @@ export default function Header() {
           <a className="mx-2 js-search-open" onClick={handleFormOpen}>
             <span className="bi-search"></span>
           </a>
+
+          {on ? (
+            <i
+              className="bi bi-x mobile-nav-toggle"
+              onClick={handleToggleMenu}
+            ></i>
+          ) : (
+            <i
+              className="bi bi-list mobile-nav-toggle"
+              onClick={handleToggleMenu}
+            ></i>
+          )}
           <SearchForm active={open} formOpen={handleFormOpen} />
         </div>
       </div>
