219dd3be144a4f672bb77456fe0be28cb7c90f55
Jeneliya12 <gurungjeneliya@gmail.com>
2024-11-25 23:11:56 -0600

    add images to homepage

--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -1,4 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import "./posts.css";
+import PostItemOne from "@/components/PostItemOne";
+
 export default function Posts() {
-  return <div>Posts</div>;
+  const router = useRouter();
+  const [items, setItems] = useState<any[]>([]);
+
+  const getItemsData = () => {
+    fetch(`/api/postitems`)
+      .then((res) => res.json()) // Added .json() to parse the response
+      .then((data) => setItems(data))
+      .catch((e) => console.error(e.message));
+  };
+
+  useEffect(() => {
+    getItemsData();
+  }, []);
+
+  return (
+    <section id="posts" className="posts">
+      <div className="container" data-aos="fade-up">
+        <div className="row g-5">
+          <div className="col-lg-4"></div>
+          <div className="col-lg-8">
+            <div className="row g-5">
+              <div className="col-lg-4 border-start custom-border">
+                {items &&
+                  items.length > 0 &&
+                  items.map(
+                    (item: {
+                      _id: string;
+                      img: string;
+                      category: string;
+                      date: string;
+                      title: string;
+                      brief: string;
+                      avatar: string;
+                      author: string;
+                    }) => (
+                      <PostItemOne key={item._id} large={false} item={item} />
+                    )
+                  )}
+              </div>
+              <div className="col-lg-4"></div>
+              <div className="col-lg-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
